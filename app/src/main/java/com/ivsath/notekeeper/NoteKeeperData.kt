e7338f35f2d9d8f14a73e6d2499b10fc1054dc24
Irvin Reyes <reyes.yerp@gmail.com>
2019-07-22 01:03:08 -0500

    Override toString method in the CourseInfo class

--- app/src/main/java/com/ivsath/notekeeper/NoteKeeperData.kt
+++ app/src/main/java/com/ivsath/notekeeper/NoteKeeperData.kt
@@ -1,5 +1,7 @@
 package com.ivsath.notekeeper
 
-class CourseInfo(val courseId: String, val title: String)
+class CourseInfo(val courseId: String, val title: String) {
+    override fun toString(): String = title
+}
 
 class NoteInfo(var course: CourseInfo, var title: String, var text: String)
